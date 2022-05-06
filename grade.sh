20502f55cf14ffc36b2d9a7b3ff43143cfbe4cfc
ddn005UCSD <103156034+ddn005UCSD@users.noreply.github.com>
2022-05-06 09:44:17 -0700

    Changed markdown parser to fit for windows

--- grade.sh
+++ grade.sh
@@ -2,5 +2,8 @@
 git clone $1
 cp GradingTests.java markdown-parser/
 cd markdown-parser
-javac -cp lib/*:. GradingTests.java MarkdownParse.java
-java -cp lib/*:. org.junit.runner.JUnitCore GradingTests
+# javac -cp lib/*:. GradingTests.java MarkdownParse.java
+# java -cp lib/*:. org.junit.runner.JUnitCore GradingTests
+
+javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" GradingTests.java MarkdownParse.java
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore GradingTests
